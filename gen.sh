9fc684308eedefce61320b475c8437a8380b995f
Louis Gesbert <louis.gesbert@ocamlpro.com>
2021-11-01 14:04:25 +0100

    Tweak gen.sh

--- gen.sh
+++ gen.sh
@@ -16,21 +16,21 @@ mkdir -p "$DIR/thumbs"
 # if [ ! -e perfectLayout.min.js ]; then
 #     wget -O perfectLayout.min.js https://raw.githubusercontent.com/axyz/perfect-layout/d56bc2d9c5212e5d79ff63994ef434a3ec2acd16/dist/perfectLayout.min.js
 # fi
-# cp index.html "$DIR"
+cp index.html "$DIR"
 # cp jquery.min.js "$DIR"
 # cp perfectLayout.min.js "$DIR"
 
 echo "[" >"$OUT"
 i=0
 for p in "${PHOTOS[@]}"; do
-    read w h orient < <(identify -format '%w %h %[EXIF:Orientation]\n' $p)
+    read w h orient < <(identify -format '%w %h %[EXIF:Orientation]\n' "$p")
     case "$orient" in
         1|3|"") RATIO=$(bc -l <<<"$w / $h") ;;
         *) RATIO=$(bc -l <<<"$h / $w")
     esac
-    base=$(basename $p)
-    convert -auto-orient -resize 1920x1920\> -interlace Plane -quality 75% "$p" "$DIR/large/$base"
-    convert -auto-orient -thumbnail 640x640\> -strip -interlace Plane -quality 64% "$p" "$DIR/thumbs/$base"
+    base=$(basename "$p")
+    convert -auto-orient -resize 1920x1920\> -interlace Plane -quality 75% "$p" "$DIR/large/$base" &
+    convert -auto-orient -thumbnail 640x640\> -strip -interlace Plane -quality 64% "$p" "$DIR/thumbs/$base" &
     if [ $i -ne 0 ]; then echo "," >>"$OUT"; fi
     LC_ALL=C printf '  { "data": "large/%s", "src": "thumbs/%s", "ratio": %f }' "$base" "$base" "$RATIO" >>"$OUT"
     i=$((i+1))
@@ -40,5 +40,7 @@ echo
 
 echo >>"$OUT"
 echo "]" >>"$OUT"
+
+wait
 
 # xdg-open "$DIR/index.html"
